fc20ea9f627001067b95dd8940fa9278460f9e3b
mdbdeepaksuv <deepakferns@gmail.com>
2025-11-24 13:46:03 +0530

    Added classname code

--- LearningGIT1.py
+++ LearningGIT1.py
@@ -1,6 +1,29 @@
-for i in range(1,10):
- if i%2==0:
-    print('even')
-else:
-    print('odd')
+# for i in range(1,10):
+#  if i%2==0:
+#     print('even')
+# else:
+#     print('odd')
+
+
+import time
+
+## Eg1
+from selenium import webdriver
+
+opts = webdriver.ChromeOptions()
+opts.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(opts)
+
+driver.get('https://demowebshop.tricentis.com/')
+time.sleep(2)
+
+driver.find_element('class name', 'ico-register').click()
+time.sleep(2)
+driver.find_element('class name', 'ico-login').click()
+time.sleep(2)
+driver.find_element('class name', 'ico-cart').click()
+
+
+
 
